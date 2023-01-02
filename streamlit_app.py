d132dfbe5403ed52fb0e6071984feabbc23e9cde
Mayur <121548634+MayurRang@users.noreply.github.com>
2023-01-02 11:24:09 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title("Welcome to Badge 2: Data Application Builders Workshop")
 sreamlit.header("Chapter 3")
-streamlit.text("At the end:)")
+streamlit.text("At the end")
