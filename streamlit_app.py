f76f61e702d5aaecb5ab3ca0dfb2a0be3fa94bd4
Mayur <121548634+MayurRang@users.noreply.github.com>
2023-01-02 11:30:02 +0530

    Update streamlit_app.py
    header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ import streamlit
 streamlit.title("My healty Menu")
 streamlit.header('🥣 Breakfast Favorites')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast)
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
